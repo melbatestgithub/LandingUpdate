3d897eef8ba16e07d1ea38cc21fa9d338108a05c
melakuzeleke <melakuzeleke443@gmail.com>
2024-06-11 14:04:02 +0300

    commit

--- src/landing/Body.js
+++ src/landing/Body.js
@@ -11,16 +11,16 @@ const Body = () => {
     <div>
       <div className="flex" id="features">
         <div>
-          <h1 className="font-extrabold text-sky-700 text-5xl p-5 pt-[100px] font-sans">
+          <h1 className="font-extrabold text-sky-700 text-4xl p-5 pt-[40px] font-sans">
             Powerful help desk software for the entire enterprise
           </h1>
           <p className="text-xl px-5 font-sans">
             Our feature-rich help desk software speeds up ticket resolution with the help of proactive automations and highly customizable workflows.
           </p>
         </div>
-        <img className="max-h-[500px]" src={DeskImg} alt="" />
+          <img className="max-h-[500px] w-[700px] " src={DeskImg} alt="" />
       </div>
-      <section  id="features">
+      <section>
         <h1 className="text-center p-5 tracking-wider text-3xl font-extrabold text-sky-900 font-sans">
           Essential features available in this help desk software
         </h1>
@@ -57,43 +57,43 @@ const Body = () => {
         <div className="mb-10">
           <div className="flex space-x-10 mx-10 mt-10">
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>What is IT support</p>
             </div>
-            <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+            <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[20px] py-3 rounded-lg font-extrabold text-2xl font-sans">
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>How to get IT support</p>
             </div>
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>How much IT cost</p>
             </div>
           </div>
           <div className="flex space-x-10 mx-10 mt-10">
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px]  h-[30px]" src={Plus} alt="" />
               <p>What is IT support</p>
             </div>
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>How to get IT support</p>
             </div>
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>How much IT cost</p>
             </div>
           </div>
           <div className="flex space-x-10 mx-10 mt-10">
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>What is IT support</p>
             </div>
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>How to get IT support</p>
             </div>
             <div className="flex space-x-3 cursor-pointer border-2 border-sky-700 text-sky-700 px-[60px] py-3 rounded-lg font-extrabold text-2xl font-sans">
-              <img className="max-w-[30px]" src={Plus} alt="" />
+              <img className="max-w-[30px] h-[30px]" src={Plus} alt="" />
               <p>How much IT cost</p>
             </div>
           </div>
