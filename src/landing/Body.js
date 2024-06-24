ed0a68fb44116d9a554418fcd8ef01483ec79763
melakuzeleke <melakuzeleke443@gmail.com>
2024-06-24 11:25:29 +0300

    commit

--- src/landing/Body.js
+++ src/landing/Body.js
@@ -10,7 +10,7 @@ const Body = () => {
   const [expandedFAQ, setExpandedFAQ] = useState(null);
 
   useEffect(() => {
-    fetch('http://it-issue-tracking-api.onrender.com/api/FAQ/getFaq')
+    fetch('https://it-issue-tracking-api.onrender.com/api/FAQ/getFaq')
       .then((response) => response.json())
       .then((data) => setFaqs(data));
   }, []);
