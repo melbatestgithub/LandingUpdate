b96be5b3b8eab4a110838daced1bda8c679bb02a
melakuzeleke <melakuzeleke443@gmail.com>
2024-06-24 18:49:10 +0300

    commit

--- src/landing/Login.js
+++ src/landing/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
   });
   const [errorMessage, setErrorMessage] = useState("");
 
- const baseUrl = "http://it-issue-tracking-api.onrender.com/api"
+ const baseUrl = "https://it-issue-tracking-api.onrender.com/api"
 
   const handleChange = (e) => {
     setLoginData({ ...loginData, [e.target.name]: e.target.value });
@@ -57,7 +57,7 @@ const Login = () => {
           headers: {
             'Content-Type': 'application/json',
           },
-          withCredentials: true,
+          withCredentials: false,
         });
         const department = res.data.others.department;
         console.log(department);
